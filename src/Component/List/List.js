e87a33bf7a94eeaf52495b16ad6e7f640c2dc062
aushalsavaliya <aushalsavaliya994@gmailcom>
2022-09-12 18:10:08 +0530

    form_Handling_12_9

--- src/Component/List/List.js
+++ src/Component/List/List.js
@@ -9,22 +9,22 @@ function List({ Data }) {
                 return (
                     <div className='col-md-4'>
                         <Card
-                        style={{
-                            width: '18rem'
-                        }}
-                    >
-                        <CardBody>
-                            <CardTitle tag="h5">
-                                {d.name}
-                            </CardTitle>
-                            <CardSubtitle
-                                className="mb-2 text-muted"
-                                tag="h6"
-                            >
-                                price: {d.price}
-                            </CardSubtitle>
-                        </CardBody>
-                    </Card>
+                            style={{
+                                width: '18rem'
+                            }}
+                        >
+                            <CardBody>
+                                <CardTitle tag="h5">
+                                    {d.name}
+                                </CardTitle>
+                                <CardSubtitle
+                                    className="mb-2 text-muted"
+                                    tag="h6"
+                                >
+                                    price: {d.price}
+                                </CardSubtitle>
+                            </CardBody>
+                        </Card>
                     </div>
                 )
             })}
--- src/Conteinars/Auth/Auth.js
+++ src/Conteinars/Auth/Auth.js
@@ -1,9 +1,42 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import * as yup from 'yup';
 
 function Auth(props) {
     const [userType, setuserType] = useState('Login');
-
     const [reset, setreset] = useState(false);
+
+    const emailref = useRef();
+    const passref = useRef();
+
+    function handeling() {
+        passref.current.focus();
+        passref.current.style.border = '2px solid black'
+        console.log(emailref.current.value);
+    }
+
+    let authschema;
+
+    if (userType === 'Singup') {
+        authschema = {
+            name: yup.string().required("please Enter your name"),
+            email: yup.string().required("please Enter Valid email").email("please Enter email"),
+            password: yup.string().required("Enter your Password").min(8),
+        }
+    } else if (userType === 'Login') {
+        authschema = {
+            email: yup.string().required("please Enter Valid email").email("please Enter email"),
+            password: yup.string().required("Enter your Password").min(8),
+        }
+    } else if(reset === true) {
+        authschema = {
+            email: yup.string().required("please Enter Valid email").email("please Enter email"),
+        }
+    }
+
+
+    let schema = yup.object().shape(authschema);
+
+
     return (
         <section id="appointment" className="appointment">
             <div className="container">
@@ -36,35 +69,35 @@ function Auth(props) {
                     </div>
                     <div className="row">
                         <div className="col-md-4 form-group mt-3 mt-md-0">
-                            <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                            <input ref={emailref} type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
                             <div className="validate" />
                         </div>
                     </div>
                     {
                         reset === true ?
-                        null
-                        :
-                        <div className="row">
-                            <div className="col-md-4 form-group mt-3 mt-md-0">
-                                <input type="tel" className="form-control" name="password" id="password" placeholder="Your password" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                <div className="validate" />
+                            null
+                            :
+                            <div className="row">
+                                <div className="col-md-4 form-group mt-3 mt-md-0">
+                                    <input ref={passref} type="tel" className="form-control" name="password" id="password" placeholder="Your password" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                    <div className="validate" />
+                                </div>
                             </div>
-                        </div>
                     }
                     {
-                        reset === true?
-                        <div className="text-center"><button type="submit">Submit</button></div>
-                        :
-                        userType === 'Login' ?
-                            <div className="text-center"><button type="submit">Login</button></div>
+                        reset === true ?
+                            <div className="text-center"><button type="submit">Submit</button></div>
                             :
-                            <div className="text-center"><button type="submit">Signup</button></div>
+                            userType === 'Login' ?
+                                <div className="text-center"><button type="submit" onClick={() => handeling()}>Login</button></div>
+                                :
+                                <div className="text-center"><button type="submit">Signup</button></div>
                     }
                     {
                         userType === 'Login' ?
-                            <div>Create a new Account <button onClick={() => {setreset(false); setuserType('Singup')}}>Signup</button></div>
+                            <div>Create a new Account <button onClick={() => { setreset(false); setuserType('Singup') }}>Signup</button></div>
                             :
-                            <div>Already have Account <button onClick={() => {setreset(false); setuserType('Login')}}>Login</button></div>
+                            <div>Already have Account <button onClick={() => { setreset(false); setuserType('Login') }}>Login</button></div>
                     }
 
                     <span>Forget password <button onClick={() => setreset(true)}>Click Here</button></span>
