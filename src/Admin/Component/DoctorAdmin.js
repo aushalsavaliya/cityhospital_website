47a66a506ab15ef949b59e5d8e984ff41a969a1b
aushalsavaliya <aushalsavaliya994@gmailcom>
2022-09-22 08:58:00 +0530

    21_9_form_dialog_validation

--- src/Admin/Component/DoctorAdmin.js
+++ src/Admin/Component/DoctorAdmin.js
@@ -6,6 +6,9 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import * as yup from 'yup';
+import { Formik, Form, useFormik } from 'formik';
+
 
 function DoctorAdmin(props) {
 
@@ -18,6 +21,32 @@ function DoctorAdmin(props) {
     const handleClose = () => {
         setOpen(false);
     };
+
+    const Doctoradd = (values) => {
+        setOpen(false);
+        formik.resetForm();
+        console.log(values);
+    }
+
+    let schema = yup.object().shape({
+        Doctor_name: yup.string().required("please enter Doctor name"),
+        Doctor_Number: yup.number().required("please enter number"),
+    });
+
+    const formik = useFormik({
+        validationSchema: schema,
+        initialValues: {
+            Doctor_name: '',
+            Doctor_Number: '',
+        },
+        onSubmit: values => {
+            Doctoradd(values);
+        },
+    });
+
+    const { handleBlur, handleChange, handleSubmit, errors, touched } = formik;
+
+
     return (
         <div>
             <h1>
@@ -29,28 +58,36 @@ function DoctorAdmin(props) {
             </Button>
             <Dialog open={open} onClose={handleClose}>
                 <DialogTitle>Subscribe</DialogTitle>
-                <DialogContent>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Doctor name"
-                        fullWidth
-                        variant="standard"
-                    />
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="text"
-                        label="Doctor Number"
-                        fullWidth
-                        variant="standard"
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleClose}>Add</Button>
-                </DialogActions>
+                <Formik values={Formik}>
+                    <Form onSubmit={handleSubmit}>
+                        <DialogContent>
+                            <TextField
+                                margin="dense"
+                                id="Doctor_name"
+                                label="Doctor_name"
+                                fullWidth
+                                variant="standard"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                            <p>{errors.Doctor_name && touched.Doctor_name ? errors.Doctor_name : ''}</p>
+                            <TextField
+                                margin="dense"
+                                id="Doctor_Number"
+                                label="Doctor_Number"
+                                fullWidth
+                                variant="standard"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                            <p>{errors.Doctor_Number && touched.Doctor_Number ? errors.Doctor_Number : ''}</p>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose}>Cancel</Button>
+                            <Button type="submit">Add</Button>
+                        </DialogActions>
+                    </Form>
+                </Formik>
             </Dialog>
         </div>
     );
--- src/Admin/Component/MedicineAdmin.js
+++ src/Admin/Component/MedicineAdmin.js
@@ -6,6 +6,8 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import * as yup from 'yup';
+import { Form, Formik, useFormik } from 'formik';
 
 
 function MedicineAdmin(props) {
@@ -19,6 +21,37 @@ function MedicineAdmin(props) {
   const handleClose = () => {
     setOpen(false);
   };
+
+  const handladd = (values) =>{
+    setOpen(false);
+   formik.resetForm();
+   console.log(values);
+  }
+
+  let schema = yup.object().shape({
+    name: yup.string().required("please enter name"),
+    Pirce: yup.number().required("please enter pirce"),
+    qnt: yup.string().required("please enter qntity"),
+    expiry: yup.string().required("please enter expiry"),
+  });
+
+  const formik = useFormik({
+    validationSchema: schema,
+    initialValues: {
+      name: '',
+      Pirce: '',
+      qnt: '',
+      expiry: '',
+    },
+    onSubmit: values => {
+      handladd(values);
+    },
+  });
+
+
+  const { handleBlur, handleChange, handleSubmit, errors, touched } = formik;
+
+
   return (
     <div>
       <h1>Medicine Admin</h1>
@@ -28,49 +61,61 @@ function MedicineAdmin(props) {
       </Button>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Add Medicine</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            name="name"
-            label="Madicine"
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="Pirce"
-            name="Pirce"
-            label="Pirce"
-            type="Pirce"
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="qnt"
-            name="qnt"
-            label="Qntity"
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="expiry"
-            name="expiry"
-            label="Expiry"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Add</Button>
-        </DialogActions>
+        <Formik values={Formik}>
+          <Form onSubmit={handleSubmit}>
+            <DialogContent>
+              <TextField
+                margin="dense"
+                id="name"
+                name="name"
+                label="Madicine"
+                fullWidth
+                variant="standard"
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              <p>{errors.name && touched.name ? errors.name : ''}</p>
+              <TextField
+                margin="dense"
+                id="Pirce"
+                name="Pirce"
+                label="Pirce"
+                fullWidth
+                variant="standard"
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              <p>{errors.Pirce && touched.Pirce ? errors.Pirce : ''}</p>
+              <TextField
+                margin="dense"
+                id="qnt"
+                name="qnt"
+                label="Qntity"
+                fullWidth
+                variant="standard"
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              <p>{errors.qnt && touched.qnt ? errors.qnt : ''}</p>
+              <TextField
+                margin="dense"
+                id="expiry"
+                name="expiry"
+                label="Expiry"
+                fullWidth
+                variant="standard"
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              <p>{errors.expiry && touched.expiry ? errors.expiry : ''}</p>
+              <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button type="submit">Add</Button>
+              </DialogActions>
+            </DialogContent>
+          </Form>
+        </Formik>
+
       </Dialog>
     </div>
 
